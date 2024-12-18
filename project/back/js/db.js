09c21d3132546fcebe372b2e73c2397d97fe75df
Роман Александрович <ra.roman88@yandex.ru>
2024-12-18 20:01:13 +0300

    r

--- project/back/js/db.js
+++ project/back/js/db.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
-    'h',
-    'postgres',
-    '1703',
+    'h', // name db
+    'postgres', // user
+    '1703', // password
     {
         host: 'localhost',
         port: 5432,
